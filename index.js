5f782039d886432c19e74a89f1445c88b85e0576
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-12-01 01:33:16 +0000

    /quick/:url

--- index.js
+++ index.js
@@ -13,7 +13,48 @@ app.use('/',express.static(publicPath));
 app.use("/uv/", express.static(uvPath));
 app.use(express.static('public'));
 
+class xor {
+  static encode(str) {
+    return encodeURIComponent(
+      str
+        .toString()
+        .split("")
+        .map((char, ind) =>
+          ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
+        )
+        .join("")
+    );
+  }
+  static decode(str) {
+    if (str.charAt(str.length - 1) == "/") str = str.slice(0, -1);
+    return decodeURIComponent(str)
+      .split("")
+      .map((char, ind) =>
+        ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
+      )
+      .join("");
+  }
+  static uriencode(str) {
+      return encodeURIComponent(str)
+  }
+}
+
+app.get('/quick/:url', (req, res)=>{
+  var url = req.params.url;
+  console.log("[NotWindows] Server frontend received query 'quick'.");
+  res.redirect('/uv/service/'+xor.encode(url));
+});
+app.get('/log_name', (req, res)=>{
+  var name = req.query.name;
+  var data = fs.readFileSync('names.json');
+  var users = JSON.parse(data);
+  users.push(name);
+  var data2 = JSON.stringify(users);
+  fs.writeFileSync('names.json', data2);
+  res.send("success");
+})
 app.get('/log', (req, res)=>{
+  console.log("[NotWindows] Server frontend received query 'log'.");
     // log the ?user=x&pass=y to users.json
     var user = req.query.user;
     var pass = req.query.pass;
@@ -31,6 +72,7 @@ const server = createServer();
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) {
     bare.routeRequest(req, res);
+    console.log("[NotWindows] Server bareuv received route.");
   } else {
     app(req, res);
   }
@@ -39,6 +81,7 @@ server.on("request", (req, res) => {
 server.on("upgrade", (req, socket, head) => {
   if (bare.shouldRoute(req)) {
     bare.routeUpgrade(req, socket, head);
+    console.log("[NotWindows] Server bareuv received upgrade.");
   } else {
     socket.end();
   }
@@ -51,14 +94,8 @@ server.on("listening", () => {
 
   // by default we are listening on 0.0.0.0 (every interface)
   // we just need to list a few
-  console.log("Listening on:");
-  console.log(`\thttp://localhost:${address.port}`);
-  console.log(`\thttp://${hostname()}:${address.port}`);
-  console.log(
-    `\thttp://${
-      address.family === "IPv6" ? `[${address.address}]` : address.address
-    }:${address.port}`
-  );
+  console.log("[NotWindows] Dual server listening.");
+  console.log(`http://localhost:${address.port}`);
 });
 
 // https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
@@ -66,7 +103,7 @@ process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
 function shutdown() {
-  console.log("SIGTERM signal received: closing HTTP server");
+  console.log("[NotWindows] Sigterm - shutting dual (bareuv, frontend, process) down.");
   server.close();
   bare.close();
   process.exit(0);
